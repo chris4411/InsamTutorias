45f904b21f81730068046ed94b796139b2047d98
LA_BF <https://github.com/Barthelemyfiacre/SpringGestionStock.git>
2022-12-16 16:24:58 +0100

    Gestion de Videos

--- resources/views/auth/layout/partials/head.blade.php
+++ resources/views/auth/layout/partials/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-<title>Auth | TutoriasInsam</title>
+<title>Auth | Tutorias Insam</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
 <meta content="Themesdesign" name="author" />
